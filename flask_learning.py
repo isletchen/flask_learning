4c4307fa97eca352c9df108d1fbdd52603545fb6
Islet Chen <czbisland@163.com>
2023-09-05 00:19:34 +0800

    Modified Structure for Web

--- flask_learning.py
+++ flask_learning.py
@@ -3,32 +3,42 @@ from flask import request,make_response,redirect,abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = "hardtoguessstring"
+
 class User():
-    def __init__(self,name,age):
+    def __init__(self,name,age,email):
         self.name = name
         self.age = age
+        self.email = email
 
-@app.route('/hello')
-def hello_world():
-    return "<h1>Hello World!</h1>"
+def dformat_date(time):
+    return time.strftime("%Y-%m-%d")
+
+app.jinja_env.filters['dform'] = dformat_date
+
+class NameForom(FlaskForm):
+    name = StringField("What is your name",validators=[DataRequired()])
+
+@app.route('/')
+def home():
+    ip_address = request.remote_addr
+    return render_template('homepage.html',current_time = datetime.utcnow(),page_type = "HomePage", ip=ip_address)
+
+@app.route('/hello/<string:name>')
+def hello_world(name):
+    return f"<h1>Hello World! {name}!</h1>"
 
 @app.route('/home')
 def show_homepage():
     return "<h1>Welcome to Homepage!</h1>"
-
-@app.route('/<string:name>')
-def show_your_name(name):
-    return f"Your Name is {name}"
-
-@app.route('/agent')
-def user_agent():
-    ua = request.headers.get('User-Agent')
-    return f"Your browser is {ua}"
 
 @app.route('/cookie')
 def response_make():
@@ -40,25 +50,29 @@ def response_make():
 def RD_baidu():
     return redirect("https://www.baidu.com/") #直接跳转到对应的baidu页面
 
+@app.route('/printhello')
+def print_helloworld():
+    ip_address = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    return render_template("user_name.html",name = "Guest",ip=ip_address,ua=user_agent,current_time = datetime.utcnow(),page_type='Hello')
+
+@app.route('/mepage')
+def myaccount():
+    usr = User("Bob",27,'xxxx@qq.com')
+    return render_template("mepage_name.html",now = datetime.now(),user=usr)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html",current_time = datetime.utcnow()),404
+
+'''
 @app.route('/usr')
 def get_user():
     id = None
     if not id:
         abort(404)
     return "We found this guy"
-
-@app.route('/printhello')
-def print_helloworld():
-    user = User("Zhibin","19")
-    return render_template("user_name.html",name=user.name)
-
-@app.route('/temp')
-def show_temp():
-    return render_template("base.html",current_time = datetime.utcnow())
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html",current_time = datetime.utcnow()),404
+'''
 
 @app.errorhandler(500)
 def internal_server_error(e):
