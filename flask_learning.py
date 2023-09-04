c7672b55b6e9a480b979429b8a5bcc3907de2885
isletchen <czbisland@163.com>
2023-09-04 11:52:34 +0800

    add flask-moment时间

--- flask_learning.py
+++ flask_learning.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 from flask import request,make_response,redirect,abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -53,11 +54,11 @@ def print_helloworld():
 
 @app.route('/temp')
 def show_temp():
-    return render_template("base.html")
+    return render_template("base.html",current_time = datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"),404
+    return render_template("404.html",current_time = datetime.utcnow()),404
 
 @app.errorhandler(500)
 def internal_server_error(e):
